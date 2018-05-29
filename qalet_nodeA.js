7d6b62926d4d729f375b04d23358c6d7eb1537bd
b2blosangeles <b2blosangeles@gmail.com>
2018-05-28 21:32:40 -0700

    Update qalet_nodeA.js

--- qalet_nodeA.js
+++ qalet_nodeA.js
@@ -97,6 +97,42 @@ server.listen(port, function() {
 	});
 });
 
+let IO = (function () {
+	
+	var obj =  function (server) {
+		let me = this;
+		me.io = require('./package/socket_io/node_modules/socket.io').listen(server);
+		let sequenceNumberByClient = new Map();		
+		me.io.on("connection", (socket) => {
+			socket.on('createRoom', function(room){
+				console.log('socket in- https')
+				console.log('socket in-1-' + socket.id + '---' + room);
+				socket.join(room, function() {
+					me.io.in('VIDEO_112').clients((err, clients) => {
+						console.log('socket in- https')
+						console.log(clients);
+					});
+				});
+				me.io.to('VIDEO_112').emit('announcements', { message: 'A new user https ' + socket.id + ' has joined!' });
+			});
+			sequenceNumberByClient.set(socket, 1);
+			socket.on("disconnect", () => {
+				sequenceNumberByClient.delete(socket);
+				me.io.in('VIDEO_112').clients((err, clients) => {
+					console.log('socket in- https')
+					console.log(clients);
+				});
+			});
+		});		
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	} 
+	
+})();
+
+
 var cert_folder = '/var/cert/sites/';
 pkg.fs.exists(cert_folder, function(exists) {
     if (exists) {
@@ -129,29 +165,8 @@ pkg.fs.exists(cert_folder, function(exists) {
 
 		https_server.listen(1443, function() {
 				console.log('Started server on port 1443 at' + new Date() + '');
-				let io =  socket_io.listen(https_server);
-				let sequenceNumberByClient = new Map();		
-				io.on("connection", (socket) => {
-					socket.on('createRoom', function(room){
-						console.log('socket in- https')
-						console.log('socket in-1-' + socket.id + '---' + room);
-						socket.join(room, function() {
-							io.in('VIDEO_112').clients((err, clients) => {
-								console.log('socket in- https')
-								console.log(clients);
-							});
-						});
-						io.to('VIDEO_112').emit('announcements', { message: 'A new user https ' + socket.id + ' has joined!' });
-					});
-					sequenceNumberByClient.set(socket, 1);
-					socket.on("disconnect", () => {
-						sequenceNumberByClient.delete(socket);
-						io.in('VIDEO_112').clients((err, clients) => {
-							console.log('socket in- https')
-							console.log(clients);
-						});
-					});
-				});				
+				let io =  new IO(https_server);
+						
 		});
 		
 	
