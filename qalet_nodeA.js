21750eb24c111be9f4ac5889637b671d3b010ff5
b2blosangeles <b2blosangeles@gmail.com>
2018-05-28 21:48:15 -0700

    Update qalet_nodeA.js

--- qalet_nodeA.js
+++ qalet_nodeA.js
@@ -132,7 +132,7 @@ pkg.fs.exists(cert_folder, function(exists) {
 
 		https_server.listen(1443, function() {
 				console.log('Started server on port 1443 at' + new Date() + '');
-				let io =  new IO(https_server);
+				let io =  new IO(env, https_server);
 						
 		});
 		
