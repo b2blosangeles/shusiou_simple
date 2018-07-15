e51a7d58e83628ae56ec81f01cd5909a7a3909c3
b2blosangeles <b2blosangeles@gmail.com>
2018-07-14 20:48:11 -0700

    Update qaletRouter.js

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -17,7 +17,7 @@
 				v.site_path = v.sites_path + '/' + 'comm';
 				return v;
 			} 
-			if(host.match(/^(www|dev|qa|)\.shusiou\.(com|win)$/)) { 
+			if(host.match(/^(www\.|dev\.|qa\.|)shusiou\.(com|win)$/)) { 
 				v.site_path = v.sites_path + '/' + 'root';
 				return v;
 			}			
