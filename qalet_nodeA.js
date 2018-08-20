6e83e654314a65c678ef91a6592a2492f8645a96
b2blosangeles <b2blosangeles@gmail.com>
2018-08-19 18:06:23 -0700

    Update qalet_nodeA.js

--- qalet_nodeA.js
+++ qalet_nodeA.js
@@ -130,7 +130,9 @@ pkg.fs.exists(ddns_path, function(exists) {
 			dnsd.createServer(function(req, res) {
 				delete require.cache[ddns_path];
 				let DDNS  = require(ddns_path + '/api/inc/ddns/ddns.js'), 
-				    ddns = new DDNS(env, _dns, ips[i]);				
+				    ddns = new DDNS(env, _dns, ips[i]);		
+				console.log('---ips[i]--->');
+				console.log(ips[i]);
 				ddns.sendRecord(req, res);
 
 			}).listen(dnsport, ips[i])
