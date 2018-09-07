234d9737c1ece1040726ff97d6965bb190918337
b2blosangeles <b2blosangeles@gmail.com>
2018-09-07 12:22:26 +0800

    Update git_auto.js

--- cron/git_auto.js
+++ cron/git_auto.js
@@ -2,5 +2,6 @@ var fs    = require('fs'), path = require('path');
 var exec = require('child_process').exec;
 var root_path =  path.join(__dirname, '..');
 var site_path =  root_path + '/sites/site';
-var cmd = 'cd ' + env.site_path + ' && git pull && cd ' + env.root_path + ' && git pull';
+console.log(site_path);
+var cmd = 'cd ' + site_path + ' && git pull && cd ' + root_path + ' && git pull';
 exec(cmd, function(error, stdout, stderr) {});
