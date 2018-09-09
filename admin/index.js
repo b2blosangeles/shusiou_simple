c9fee872d0b0f109ce4ab823b39dfd173d35e26a
b2blosangeles <b2blosangeles@gmail.com>
2018-09-10 06:57:29 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -1,4 +1,11 @@
 var crypto = require('crypto');
+function cryptPwd(password) {
+    var md5 = crypto.createHash('md5');
+    return md5.update(password).digest('hex');
+}
+
+var password = '123456';
+res.send(cryptPwd(password));
 var me = this, fn = env.root_path + '/admin/' + __path;
 pkg.fs.exists(fn, function(exists) {
   if (exists) {
