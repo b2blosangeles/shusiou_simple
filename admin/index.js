8eb1c6d82738d5fc055ba59cdc6fb06ac36c19c9
b2blosangeles <b2blosangeles@gmail.com>
2018-09-10 06:06:15 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -1,1 +1,8 @@
-res.send(__path);
+var me = this, fn = env.root_path + '/admin/' + _path;
+pkg.fs.exists(fn, function(exists) {
+  if (exists) {
+    res.sendFile(fn); 									
+  } else {
+    me.send404(v);					
+  } 
+});
