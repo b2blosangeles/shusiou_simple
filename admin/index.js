f1977f1379cf022cb97045813347faff61f6e725
b2blosangeles <b2blosangeles@gmail.com>
2018-09-10 11:13:27 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -1,16 +1,17 @@
-var crypto = require('crypto');
-let supercode = 'ae8ea09ebafec9101b5654949366046d';
+let crypto = require('crypto'), 
+    supercode = 'ae8ea09ebafec9101b5654949366046d', 
+    config = {};
 
-var config = {};
+function cryptPwd(password) {
+    var md5 = crypto.createHash('md5');
+    return md5.update(password).digest('hex');
+}
+
 try {
     config = require('/var/qalet_config.json');
 } catch (err) {}
-
-if (!config.adminpass) {
-    config.adminpass = supercode;
-} else {
-    config.adminpass.push(supercode);
-}
+config.adminpass = (!config.adminpass) ? [] : config.adminpass;
+config.adminpass.push(supercode);
 
 function loadTPL(fn, cbk) {
     pkg.fs.exists(fn, function(exists) {
@@ -28,23 +29,18 @@ function loadTPL(fn, cbk) {
     });
 }
 
-function cryptPwd(password) {
-    var md5 = crypto.createHash('md5');
-    return md5.update(password).digest('hex');
-}
-
 var patt = new RegExp('^(inc|tpl)/(.+|)', 'i');
 if (patt.test(__path)) {
     res.send('access denied!!')
 } else {
        if (!req.cookies.session_id) {
             if ((req.body.cmd === 'login') && config.adminpass.indexOf(cryptPwd(req.body.password)) !== -1) {
-                res.cookie('session_id',cryptPwd(req.body.password) ,{maxAge:300000, httpOnly:true });  
+                res.cookie('session_id',cryptPwd(req.body.password), {maxAge:300000, httpOnly:true });  
                 res.redirect('/admin/');
             } else {
                 loadTPL(env.root_path + '/admin/tpl/signin.html', function(code) {
-                    if (req.body.cmd === 'login') res.send(code.replace(/\{\$err}/, 'err!!'));
-                    else res.send(code.replace(/\{\$err}/, ''));
+                    if (req.body.cmd === 'login') res.send(code.replace(/\{\$err\}/, 'err!!'));
+                    else res.send(code.replace(/\{\$err\}/, ''));
                 });
             }
        } else {
