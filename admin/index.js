a005b3a45a24f1473644dba6354a9fee31955577
b2blosangeles <b2blosangeles@gmail.com>
2018-09-11 01:11:32 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -41,9 +41,8 @@ if (patt.test(__path)) {
                 res.redirect('/admin/');
             } else {
                 loadTPL(env.root_path + '/admin/tpl/signin.html', function(code) {
-                    res.send(code);
-                    //if (req.body.cmd === 'login') res.send(code.replace(/\{\$err\}/, 'err!!'));
-                   // else res.send(code.replace(/\{\$err\}/, ''));
+                    if (req.body.cmd === 'login') res.send(code.replace(/\{\$err\}/, 'err!!'));
+                    else res.send(code.replace(/\{\$err\}/, ''));
                 });
             }
        } else {
