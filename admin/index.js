ad7129d1a1ec9647f33f9925f529a96efb68789d
b2blosangeles <b2blosangeles@gmail.com>
2018-09-12 05:45:33 +0800

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -1,7 +1,3 @@
-let crypto = require('crypto'), 
-    supercode = 'ae8ea09ebafec9101b5654949366046d', 
-    config = {};
-
 function loadTPL(fn, cbk) {
     pkg.fs.exists(fn, function(exists) {
       if (exists) {
@@ -18,52 +14,23 @@ function loadTPL(fn, cbk) {
     });
 }
 
-
-try {
-    delete require.cache['/var/qalet_config.json'];
-    config = require('/var/qalet_config.json');
-} catch (err) {}
-config.adminpass = (!config.adminpass) ? [] : config.adminpass;
-config.adminpass.push(supercode);
-
-delete require.cache[env.root_path + '/admin/inc/auth/auth.js'];
-var AUTH = require(env.root_path + '/admin/inc/auth/auth.js');
-var auth = new AUTH(res, req, env, pkg, config, loadTPL, __path);
-auth.check();
-return true;
-
-
 var patt = new RegExp('^(inc|tpl)/(.+|)', 'i');
 if (patt.test(__path)) {
     res.send('access denied!!')
 } else {
-        var md5 = cryptPwd((req.body.password)?req.body.password:'');
-        switch (req.body.authCmd) {
-            case 'login':
-                if (config.adminpass.indexOf(md5) !== -1) {
-                    res.cookie('session_id',md5, {maxAge:300000, httpOnly:true }); 
-                    res.redirect('/admin/');
-                } else {
-                     loadTPL(env.root_path + '/admin/tpl/signin.html', function(code) {
-                        res.send(code.replace(/\{\$err\}/ig, 'err!!'));
-                    });                        
-                }
-                break;
-
-            case 'signout': 
-                res.clearCookie('session_id');
-                res.redirect('/admin/');
-                break;
-
-            default :
-                if ((req.cookies.session_id) && config.adminpass.indexOf(req.cookies.session_id) !== -1) {
-                    loadTPL(env.root_path + '/admin/tpl/mainpage.html', function(code) {
-                        res.send(code);
-                    });
-                } else {
-                    loadTPL(env.root_path + '/admin/tpl/signin.html', function(code) {
-                        res.send(code.replace(/\{\$err\}/ig, ''));
-                    });
-                }
-        }
+    let crypto = require('crypto'), 
+        supercode = 'ae8ea09ebafec9101b5654949366046d', 
+        config = {};
+    
+    try {
+        delete require.cache['/var/qalet_config.json'];
+        config = require('/var/qalet_config.json');
+    } catch (err) {}
+    config.adminpass = (!config.adminpass) ? [] : config.adminpass;
+    config.adminpass.push(supercode);
+    
+    delete require.cache[env.root_path + '/admin/inc/auth/auth.js'];
+    var AUTH = require(env.root_path + '/admin/inc/auth/auth.js');
+    var auth = new AUTH(res, req, env, pkg, config, loadTPL, __path);
+    auth.check();
 }
