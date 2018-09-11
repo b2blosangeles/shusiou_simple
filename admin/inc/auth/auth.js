e7b4cef41a8527a63a7d5d1b2396c90612c3ee1c
b2blosangeles <b2blosangeles@gmail.com>
2018-09-12 00:22:23 +0800

    Update auth.js

--- admin/inc/auth/auth.js
+++ admin/inc/auth/auth.js
@@ -1,10 +1,10 @@
-(function (res, req) { 
-	var obj =  function () {
+(function () { 
+	var obj =  function (res, req) {
 		this.check = function() {
 			res.send('AAA_' + __path);
 			// var fs = require('fs');
 		};	
 	};
 	module.exports = obj;
-})(res, req);
+})();
 
