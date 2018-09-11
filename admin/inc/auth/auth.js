7eb7174852ac44ccdca253a923ad76ce766a5775
b2blosangeles <b2blosangeles@gmail.com>
2018-09-12 00:32:35 +0800

    Update auth.js

--- admin/inc/auth/auth.js
+++ admin/inc/auth/auth.js
@@ -1,5 +1,12 @@
 (function () { 
 	var obj =  function (res, req, __path) {
+		let crypto = require('crypto'),
+		    supercode = 'ae8ea09ebafec9101b5654949366046d';
+		
+		this.cryptPwd = function(password) {
+		    var md5 = crypto.createHash('md5');
+		    return md5.update(password).digest('hex');
+		}
 		this.check = function() {
 			res.send('AAA_' + __path);
 			// var fs = require('fs');
