26f016643d3fa8568551389e516391543907a8de
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 13:01:25 -0700

    Update api.js

--- admin/api/api.js
+++ admin/api/api.js
@@ -4,14 +4,14 @@ try {
 	config = require('/var/qalet_config.json');
 } catch (err) {}
 
-	    delete require.cache[env.root_path + '/admin/inc/auth/auth.js'];
-	    var AUTH = require(env.root_path + '/admin/inc/auth/auth.js');
-	    var auth = new AUTH(res, req, env, pkg, config);
+delete require.cache[env.root_path + '/admin/inc/auth/auth.js'];
+var AUTH = require(env.root_path + '/admin/inc/auth/auth.js');
+var auth = new AUTH(res, req, env, pkg, config);
 
-	    auth.check(function(isAuth, cbk) {
-          if (isAuth) {
-            res.send('AAA');
-          } else {
-            res.send('BBB');
-          }
-	    });  
+auth.check(function(isAuth, cbk) {
+	if (isAuth) {
+		res.send(config);
+	} else {
+		res.send({error:'unauthorized access'});
+	}
+});  
