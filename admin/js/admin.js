21064f88e36c56a2adb29687bf30ea38af172d8e
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 14:24:13 -0700

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,3 +1,8 @@
+function control(cmd, db_id) {
+  $('#main_admin_control').find('input[name=appCmd]').val(cmd);
+  $('#main_admin_control').find('input[name=dbid]').val(db_id);
+  $('#main_admin_control').submit();
+}
 $(document).ready(function() {
     $.post( "/admin/api/api.api", {cmd:'getDBMenu'}).done(function( data ) {
          $('#database_menu').html(data);
