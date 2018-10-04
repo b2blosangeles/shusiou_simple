76be8f2432934bca34cdc87d1404ead6da6c83d4
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 12:43:27 -0700

    Update qaletRouter.js

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -49,7 +49,7 @@
 		};
 		this.runAdmin = function(v) {
 			var me = this;
-			var patt = new RegExp('/api/(.+|)', 'i');
+			var patt = new RegExp('api/(.+|)', 'i');
 			if (patt.test(v)) {
 				res.send(v + '===niu');
 				return true;
