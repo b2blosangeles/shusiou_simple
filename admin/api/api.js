7c921634677b4f8d6c865a265fa7544fb4af0292
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 12:55:11 -0700

    Update api.js

--- admin/api/api.js
+++ admin/api/api.js
@@ -1,1 +1,12 @@
-res.send('AAA');
+
+	    delete require.cache[env.root_path + '/admin/inc/auth/auth.js'];
+	    var AUTH = require(env.root_path + '/admin/inc/auth/auth.js');
+	    var auth = new AUTH(res, req, env, pkg, config);
+
+	    auth.check(function(isAuth, cbk) {
+          if (isAuth) {
+            res.send('AAA');
+          } else {
+            res.send('BBB');
+          }
+	    });  
