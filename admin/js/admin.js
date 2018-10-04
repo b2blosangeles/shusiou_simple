067b020e964d44597168b9dd3098b41e5a0fd2e0
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 16:57:06 -0700

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -19,7 +19,7 @@ function loadDBModule(cdb) {
     }); 
 }
 function saveDBCFG() {
-    alert($('#DBCFG_FORM')[0].serialize());
+    alert($('#DBCFG_FORM').serialize());
     return true;
     $.post( "/admin/api/api.api", {
           cmd:'getDBModule', 
