4e93afbc63328280fa7a5b5da888fe992257bc95
b2blosangeles <b2blosangeles@gmail.com>
2018-10-04 14:19:37 -0700

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $.post( "/admin/api/api.api", {cmd:'getDBMenu'}).done(function( data ) {
-         $('#module_menu').html(data);
+         $('#database_menu').html(data);
     });
 });
